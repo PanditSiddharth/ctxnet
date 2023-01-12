fe4a8ec7b25c445648aaaf8a10e2277151a9d235
Sid <mynmss03@gmail.com>
2023-01-12 14:35:39 +0530

    h3

--- functions/bot/start.js
+++ functions/bot/start.js
@@ -1,7 +1,7 @@
 function strt(bot){
 
 bot.help(ctx => {
-    console.log("Received /start command")
+    console.log("Received /help command")
     try {
       ctx.reply("help command")
     } catch (e) {
@@ -10,6 +10,30 @@ bot.help(ctx => {
     }
   })
 
+  try {
+
+    const keyboard = [
+      [{ text: 'Option 1', callback_data: '1' }, { text: 'Option 2', callback_data: '2' }],
+      [{ text: 'Option 3', callback_data: '3' }, { text: 'Option 4', callback_data: '4' }]
+    ];
+    const reply_markup = {
+      inline_keyboard: keyboard
+    };
+
+    bot.command('btn', async (ctx) => await ctx.telegram.sendMessage(
+      ctx.message.chat.id,
+      'Like?',
+      { reply_markup })
+    )
+
+    bot.action('1', (ctx) => ctx.editMessageText('🎉 Awesome! 🎉'))
+    bot.action('2', (ctx) => ctx.editMessageText('okey2'))
+    bot.action('3', (ctx) => ctx.editMessageText('okey3'))
+    bot.action('4', (ctx) => ctx.editMessageText('okey4'))
+
+}catch(e){
+    console.log('buttons error')
 }
 
+}
 module.exports = { strt }
