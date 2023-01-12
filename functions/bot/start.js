eb34632d213eac0754bf051ab62bd5595ea57be9
Sid <mynmss03@gmail.com>
2023-01-12 22:09:47 +0530

    h3

--- functions/bot/start.js
+++ functions/bot/start.js
@@ -30,10 +30,34 @@ bot.help(ctx => {
     bot.action('2', (ctx) => ctx.editMessageText('okey2'))
     bot.action('3', (ctx) => ctx.editMessageText('okey3'))
     bot.action('4', (ctx) => ctx.editMessageText('okey4'))
+    r(bot)
 
+    // bot.on('new_chat_members', (ctx)=>{ctx.sendMessage('welcome')});
 }catch(e){
     console.log('buttons error')
 }
 
 }
+
+const r = async (bot) => {
+    try {
+        await bot.on('new_chat_members', (ctx) => { ctx.reply('welcome') 
+        console.log('no error')
+    }
+    )
+} catch (error) {
+    console.log(error)
+}}
+
 module.exports = { strt }
+   
+
+// const welcomeMessage = (ctx) => {
+//     const { new_chat_members } = ctx.message;
+//     const newMembers = new_chat_members.filter( member => member.is_bot === false)
+//     var membersName = newMembers.map( usr =>{ 
+//       let fullname = [usr.first_name,usr.last_name].join(" ");
+//       return fullname;
+//     });
+//     ctx.replyWithMarkdown(`Welcom ${membersName.join(', ')}`);
+//   }
