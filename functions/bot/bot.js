c73ed30dd78ee7cd104fa19ce71ff87d85662d0f
sidhelpbot <mynmss03@gmail.com>
2023-01-13 13:55:01 +0530

    h3

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -17,8 +17,28 @@ const bot = new Telegraf('5914579167:AAHBhbD4JY3IhOPdk-bncrKQHzv3BEUYdmc', {
       'chat_member',
       'chat_join_request'
   ],
-  dropPendingUpdates: false, // Don't activate this
+  dropPendingUpdates: false,// Don't activate this
+  channelMode : true,
 })
+
+    bot.on("chat_member", async ctx => {
+      try {
+        var new_chat_member = ctx.update.chat_member.new_chat_member;
+        let chat = ctx.update.chat_member.chat;
+
+        if (new_chat_member.status == 'left') {
+          await ctx.reply('Sed, ' + new_chat_member.user.first_name + '!! has left this group')
+
+        }
+        else if (new_chat_member.status == 'member' && !new_chat_member.user.is_bot) {
+          await ctx.reply('Hi, ' + new_chat_member.user.first_name + '!! Welcome in group')
+          console.log(new_chat_member)
+        }
+      } catch (error) {
+        console.error('too many requests', error);
+      }
+    });
+
 const st = require('./start.js')
 
 bot.start(ctx => {
