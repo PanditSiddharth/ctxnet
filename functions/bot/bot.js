57fff322d5d8c9a5e7ea0f5b21afdc7c41646399
Siddharth Sharma <75090218+sidhelpbot@users.noreply.github.com>
2023-01-14 00:35:28 +0530

    Update bot.js

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -23,7 +23,7 @@ bot.telegram.setWebhook('https://ctxnet.netlify.app/api/bot', {
     dropPendingUpdates: false, // Don't activate this
   })
 
-   bot.startWebhook(`/`, null, 4000);
+   bot.startWebhook(`/api/bot`, null, 4000);
 
     bot.on('chat_member', async ctx => {
       try {
