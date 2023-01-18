ec3773b823f7b01d9f34102aefe3692c1a6fb9a8
Siddharth Sharma <75090218+sidhelpbot@users.noreply.github.com>
2023-01-18 19:33:13 +0530

    Update bot.js

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -3,33 +3,33 @@ const bot = new Telegraf(process.env.MY_BOT_TOKEN)
 const st = require('./start.js')
 const { message } = require('telegraf/filters');
 
-bot.start(ctx => {
-  try {
-     let arr = [
-     `Welcome ${ctx.message.from.first_name}`,
-       `bot is starting for you..`,
-       'Please wait...`,
-       'Only 3 seconds...',
-       'Only 2 seconds...',
-       'Only 1 seconds...',
-       'Bot Started..  :)',
-       'Please enter any pincode to see details\nExample: send 226101'
-     ]
-
-      let i = 0;
-      let i_d = await setInterval(async () => {
-
-        if (i > 7)
-          clearInterval(i_d);
-         try {
-        await bot.telegram.editMessageText('@shabdt', y.message_id, undefined, arr[i++]);
-      } catch (error) {}
-          }, 1000)
-        
-  } catch (e) {
-    ctx.reply("Error occured")
-  }
-})
+    bot.start(async ctx => {
+      try {
+         let arr = [
+           'Welcome ' + ctx.message.from.first_name,
+           'bot is starting for you..',
+           'Please wait...',
+           'Only 3 seconds...',
+           'Only 2 seconds...',
+           'Only 1 seconds...',
+           'Bot Started..  :)',
+           'Please enter any pincode to see details\nExample: send 226101'
+         ]
+    
+          let i = 0;
+          let i_d = await setInterval(async () => {
+    
+            if (i > 7)
+              clearInterval(i_d);
+             try {
+            await bot.telegram.editMessageText('@shabdt', y.message_id, undefined, arr[i++]);
+          } catch (error) {}
+              }, 1000)
+            
+      } catch (e) {
+        ctx.reply("Error occured")
+      }
+    })
 
 exports.handler = async (event, context, callback) => {
   try {
